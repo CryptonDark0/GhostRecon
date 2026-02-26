a476f142c8d23d9bfa05cc6623add1137f51658c
cryptondark0 <98961170+CryptonDark0@users.noreply.github.com>
2026-02-26 11:42:51 +0545

    Create firebaseConfig.js

---
+++ frontend/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "ghostrecon-9c294.firebaseapp.com",
+  projectId: "ghostrecon-9c294",
+  storageBucket: "ghostrecon-9c294.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
